58ba59322e48d231b64150bb2db2af8127385e44
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2022-05-01 00:41:40 -0700

    feat: update installer

--- install.sh
+++ install.sh
@@ -2,5 +2,17 @@
 
 set -e
 
-cp -rvn bin lib "$HOME" \
-  && find . -type f \( -path "./dotfiles/*" -a -iname ".*" \) -exec cp -rvn '{}' "$HOME" ';'
+# param1 - home dir
+# param2 - mpv config dir
+install_dotfiles() {
+  mkdir -p "$@" \
+    && cp -rv bin lib "$1" \
+    && cp -rv ./dotfiles/mpv.conf "$2" \
+    && find . -type f \( -path "./dotfiles/*" -a -iname ".*" \) -exec cp -rv '{}' "$1" ';'
+}
+
+if [[ "$#" -lt 2 ]]; then
+  install_dotfiles "$HOME" "$HOME/.config/mpv"
+else
+  install_dotfiles "$@"
+fi
